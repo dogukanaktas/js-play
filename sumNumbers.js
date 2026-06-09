7751bd0b3c7f9fa178030b82167205bfa134175b
Dogukan Aktas <aktasdogukann@gmail.com>
2026-06-09 18:32:59 +0300

    Add sumNumbers solution

---
+++ sumNumbers.js
@@ -0,0 +1,12 @@
+// 1'den 100'e kadar olan sayıların toplamını hesapla, ama 3'e bölünebilenleri hariç tut. Sonuç ne olmalı?
+
+// const sumNumbers = () =>
+//   Array.from({ length: 100 }, (_, i) => i + 1).reduce(
+//     (prev, curr) => (curr % 3 !== 0 ? prev + curr : prev),
+//     0,
+//   );
+
+
+
+
+// console.log(sumNumbers());
